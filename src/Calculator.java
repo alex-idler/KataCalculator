50965223466af2c5ae6a529c63c08028f198cb4f
alex-idler <107202057+alex-idler@users.noreply.github.com>
2022-06-09 19:35:53 +0300

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -57,23 +57,14 @@ public class Calculator {
     }
 
     private static int toArabic(String romanNumber) throws Exception {
-        String[] romanNumbers = {"","I","II","III","IV","V","VI","VII","VIII","IX","X",
-                "XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX",
-                "XXI","XXII","XXIII","XXIV","XXV","XXVI","XXVII","XXVIII","XXIX","XXX",
-                "XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX","XL",
-                "XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII","XLIX","L",
-                "LI","LII","LIII","LIV","LV","LVI","LVII","LVIII","LIX","LX",
-                "LXI","LXII","LXIII","LXIV","LXV","LXVI","LXVII","LXVIII","LXIX","LXX",
-                "LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI","LXXVII","LXXVIII","LXXIX","LXXX",
-                "LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC",
-                "XCI","XCII","XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C"};
+        String[] romanNumbers = {"","I","II","III","IV","V","VI","VII","VIII","IX","X"};
 
         for (int i=0; i<romanNumbers.length; i++) {
             if (romanNumbers[i].equals(romanNumber)) {
                 return i;
             }
         }
-        throw new Exception("Одно из чисел записано неверно.");
+        throw new Exception("Одно из чисел больше 10 либо записано неверно.");
     }
 
     private static String toRoman(int arabic) {
